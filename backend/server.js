0697b0ef35e9fb031dfcbb226339a1a2207c32ed
Farhan Ali <mernstackdeveloper@yahoo.com>
2022-09-18 00:37:24 +0500

    publish to heroku

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+
+
+const app = express();
+const port = 5555
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/innerfolder/build')));
+app.use('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/innerfolder/build/index.html'))
+});
+
+app.listen(port, ()=>{
+    console.log(`server is running on ${port}`)
+})
