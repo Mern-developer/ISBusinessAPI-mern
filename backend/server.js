be34c356b27970bf3e1e666205a616a8a3199f4d
Farhan Ali <mernstackdeveloper@yahoo.com>
2022-09-18 22:40:26 +0500

    set env

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv')
 
-
-
+dotenv.config()
 const app = express();
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/innerfolder/build')));
-app.get('*', (req, res)=>{
+app.get('/*', (req, res)=>{
     res.sendFile(path.join(__dirname, '/innerfolder/build/index.html'))
 });
 
-const port = 5432
+const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log(`server is running on ${port}`)
 })
