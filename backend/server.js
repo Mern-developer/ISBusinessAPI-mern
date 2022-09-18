45d0e6ebf483209485aa045c9a5899c51dd3d322
Farhan Ali <mernstackdeveloper@yahoo.com>
2022-09-18 22:03:54 +0500

    Changed

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,14 @@ const path = require('path');
 
 
 const app = express();
-const port = 5555
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/innerfolder/build')));
-app.get('/', (req, res)=>{
+app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, '/innerfolder/build/index.html'))
 });
 
+const port = 5432
 app.listen(port, ()=>{
     console.log(`server is running on ${port}`)
 })
