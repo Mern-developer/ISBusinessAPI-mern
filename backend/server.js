aa82619a05fb022d1678711005675c1c27640897
Farhan Ali <mernstackdeveloper@yahoo.com>
2022-09-18 11:57:56 +0500

    some change to publish

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const port = 5555
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/innerfolder/build')));
-app.use('*', (req, res)=>{
+app.use('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '/innerfolder/build/index.html'))
 });
 
